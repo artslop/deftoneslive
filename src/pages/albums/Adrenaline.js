e2965c7bd94966150af0d456a6b632dadfc9e20a
artslop <cpolstra@gmail.com>
2022-08-28 22:29:48 -0400

    Album Page Layout & Styles

--- src/pages/albums/Adrenaline.js
+++ src/pages/albums/Adrenaline.js
@@ -9,19 +9,26 @@ function Adrenaline() {
             <Header />
             <div id="common">
                 <h2>Adrenaline</h2>
-                <ol>
-                    <li>Bored</li>
-                    <li>Minus Blindfold</li>
-                    <li>One Weak</li>
-                    <li>Nosebleed</li>
-                    <li>Lifter</li>
-                    <li>Root</li>
-                    <li>7 Words</li>
-                    <li>Birthmark</li>
-                    <li>Engine No. 9</li>
-                    <li>Fireal</li>
-                    <li>Fist (hidden track)</li>
-                </ol>
+                <div id="albumDetails">
+                    <div class="album-art">
+                        <img src="../albumcovers/Adrenaline.jpg" alt="Adrenaline" />
+                    </div>
+                    <div class="track-list">
+                        <ol>
+                            <li>Bored</li>
+                            <li>Minus Blindfold</li>
+                            <li>One Weak</li>
+                            <li>Nosebleed</li>
+                            <li>Lifter</li>
+                            <li>Root</li>
+                            <li>7 Words</li>
+                            <li>Birthmark</li>
+                            <li>Engine No. 9</li>
+                            <li>Fireal</li>
+                            <li>Fist (hidden track)</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
