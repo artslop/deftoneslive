2fd9c489a7e0460026a1a39de42dbfe9c7e59fde
Michael <mikerooze12@gmail.com>
2022-09-10 21:38:02 -0400

    Reformat AllTracks and remove strict mode

--- src/components/AllTracks.js
+++ src/components/AllTracks.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid'
 import { useDispatch } from 'react-redux';
 import { addSongDetails } from '../features/shows/showsSlice';
@@ -21,56 +20,54 @@ function AllTracks({ showTrackList, encore1, encore2, info, goBack }) {
         navigate("../songdetails");
     }
 
+    let trackList =
+        showTrackList
+            .map((track, idx) => {
+                return (
+                    <div key={nanoid()} className="allTracks" >
+                        <li onClick={() => { handleDisplaySongDetails(track) }}>
+                            {`${idx + 1}. ${track}`}
+                        </li>
+                    </div>
+                )
+            })
+
+    let encoreOne = encore1.map((encore, idx) => {
+        return (
+            <div key={nanoid()} className="allTracks">
+                <li>{`${showTrackList.length + (idx + 1)}. ${encore}`}</li>
+            </div>
+        )
+    })
+
+    let encoreTwo = encore2.map((encore2, idx) => {
+        let numOfTracks = showTrackList.length + encore1.length;
+        return (
+            <div key={nanoid()} className="allTracks">
+                <li>{`${numOfTracks + (idx + 1)}. ${encore2}`}</li>
+            </div>
+        )
+    })
+
     return (
         <div>
             <div className="tablecontain">
                 <div className="setlist__container">
                     <div>
                         <h4>Main Set</h4><button className='btn__goBack' onClick={goBack}>Go Back</button>
-
-                        {
-                            showTrackList
-                                .map((track, idx) => {
-                                    return (
-                                        <div key={nanoid()} className="allTracks" >
-                                            <li onClick={() => { handleDisplaySongDetails(track) }}>
-                                                {`${idx + 1}. ${track}`}
-                                            </li>
-                                        </div>
-                                    )
-                                })
-                        }
+                        {trackList}
                     </div>
                     <div className="encore">
                         {encore1.length > 0 &&
                             <h4>Encore 1</h4>
                         }
-
-                        {
-                            encore1.map((encore, idx) => {
-                                return (
-                                    <div key={nanoid()} className="allTracks">
-                                        <li>{`${showTrackList.length + (idx + 1)}. ${encore}`}</li>
-                                    </div>
-                                )
-                            })
-                        }
+                        {encoreOne}
                     </div>
                     <div className="encore">
                         {encore2.length > 0 &&
                             <h4>Encore 2</h4>
                         }
-
-                        {
-                            encore2.map((encore2, idx) => {
-                                let numOfTracks = showTrackList.length + encore1.length;
-                                return (
-                                    <div key={nanoid()} className="allTracks">
-                                        <li>{`${numOfTracks + (idx + 1)}. ${encore2}`}</li>
-                                    </div>
-                                )
-                            })
-                        }
+                        {encoreTwo}
                         {
                             info !== undefined &&
                             <>
@@ -78,7 +75,6 @@ function AllTracks({ showTrackList, encore1, encore2, info, goBack }) {
                                 <div className="allTracks">{info}</div>
                             </>
                         }
-
                     </div>
                 </div>
             </div>
